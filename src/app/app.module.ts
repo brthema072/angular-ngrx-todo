745d7862341961bb344912021aa818558c3ed72c
brthema072 <brunomarthe@hotmail.com>
2023-04-22 00:14:04 -0300

    Add ngrx store and devTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,10 +7,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { TodosComponent } from './pages/todos/todos.component';
 import { AboutComponent } from './pages/about/about.component';
 import { YesNoPipe } from './pipes/yes-no.pipe';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent, TodosComponent, AboutComponent, YesNoPipe],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
